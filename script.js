182fb88914a38976cea76ac02345e622e130f0cb
Jessica Zeng <sicazeng@gmail.com>
2026-02-08 22:01:48 -0800

    add index file

---
+++ script.js
@@ -0,0 +1,12 @@
+// Load YouTube IFrame Player API code asynchronously.
+function showMessage(response) {
+  let videoPlayed = false;
+  if (response === "No") {
+    console.log("Sad");
+  }
+
+  if (response === "Yes") {
+    console.log("Happy");
+  }
+
+}
