cf06cf83cfda8e85c468030a5ed303223c4d0b2d
Jessica Zeng <sicazeng@gmail.com>
2026-02-10 23:23:46 -0800

    updated question flow

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function moveToQuestion(current_question_number, next_question_number) {
   document.getElementById('q' + current_question_number).classList.add('hidden');
   document.getElementById('q' + next_question_number).classList.remove('hidden');
 }
+
+function moveToFinalQuestion() {
+  document.getElementById('q4').classList.add('hidden');
+  document.getElementById('final-q').classList.remove('hidden');
+}
