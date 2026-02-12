28892fbb3980fd25a711f2cbb1724f075cf67513
Jessica Zeng <sicazeng@gmail.com>
2026-02-12 13:17:33 -0800

    add backpack purchase count

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function moveToQuestion(current_question_number, next_question_number) {
+let num_backpacks = 0;
+let total_cost = 0;
+const backpack_price = 8;
+
+function moveToQuestion(current_question_number, next_question_number, buy_backpack=false) {
   document.getElementById('q' + current_question_number).classList.add('hidden');
   document.getElementById('q' + next_question_number).classList.remove('hidden');
+  if (buy_backpack) {
+    buyBackpack();
+  }
 }
 
 function moveToFinalQuestion() {
@@ -24,4 +31,15 @@ function transitionImageToQuestions(current_question_number, next_question_numbe
       document.getElementById('question-container').classList.remove('hidden');
       moveToQuestion(current_question_number, next_question_number);
   }, durationInMilliseconds);
+}
+
+function buyBackpack() {
+  num_backpacks++;
+  total_cost = total_cost + backpack_price;
+
+  var num_backpacks_element = document.getElementById("num-backpacks");
+  num_backpacks_element.innerHTML = num_backpacks.toString();
+
+  var total_cost_element = document.getElementById("total-cost");
+  total_cost_element.innerHTML = "$" + total_cost.toString();
 }
