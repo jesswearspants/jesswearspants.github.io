529498404f660bf3f3400f86e5cd12a026e0e761
Jessica Zeng <sicazeng@gmail.com>
2026-02-12 09:56:45 -0800

    add transition images

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function moveToFinalQuestion() {
   document.getElementById('q4').classList.add('hidden');
   document.getElementById('final-q').classList.remove('hidden');
 }
+
+function transitionImageToQuestions(current_question_number, next_question_number, image_id) {
+  const imageElement = document.getElementById(image_id);
+  const durationInMilliseconds = 3000; // 3 seconds
+
+  document.getElementById('question-container').classList.add('hidden');
+
+  // Show the image
+  imageElement.style.display = 'block';
+  imageElement.style.animation = 'shrink 3s ease-out forwards';
+
+  // Set a timeout to hide the image after the specified duration
+  setTimeout(function() {
+      imageElement.style.display = 'none';
+      document.getElementById('question-container').classList.remove('hidden');
+      moveToQuestion(current_question_number, next_question_number);
+  }, durationInMilliseconds);
+}
