d5b4739c4d654330ee7abd32f41970cded0bfa90
Jessica Zeng <sicazeng@gmail.com>
2026-02-11 19:19:43 -0800

    add button images

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function moveToQuestion(current_question_number, next_question_number) {
-  console.log("Current question number: " + current_question_number);
-  console.log("Next question number: " + next_question_number);
-  if (next_question_number == -1) {
-    return;
-  }
   document.getElementById('q' + current_question_number).classList.add('hidden');
   document.getElementById('q' + next_question_number).classList.remove('hidden');
 }
